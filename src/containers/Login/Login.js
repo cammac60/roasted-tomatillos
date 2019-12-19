b54c3de6fad7894caccee72bae818078399f866e
Cameron MacRae <cam.mac.cm@gmail.com>
2019-12-19 12:37:11 -0700

    Add form validation

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -22,7 +22,7 @@ class Login extends Component {
           <input id="email" value={this.state.username} type="text" onChange={(event) => this.handleChange(event)}/>
           <label>Password:</label>
           <input id="password" value={this.state.password} type="password" onChange={(event) => this.handleChange(event)}/>
-          <button className="sign-in">Sign in</button>
+          <button className="sign-in" onClick={(event) => this.handleSubmit(event)}>Sign in</button>
           <Link id="back-home" to="/">Go back</Link>
         </div>
       </form>
@@ -31,6 +31,7 @@ class Login extends Component {
 
   handleChange = (event) => {
     let { target } = event;
+    this.validateForm();
     switch(target.id) {
       case 'email':
         this.setState({email: target.value})
@@ -40,6 +41,25 @@ class Login extends Component {
         break;
       default:
         console.log('hits default');
+    }
+  }
+
+  validateForm = () => {
+    if (this.state.email && this.state.password) {
+      this.setState({errorMsg: ''});
+      return true;
+    } else {
+      this.setState({errorMsg: 'Please enter an email and a password'});
+      return false;
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.validateForm()) {
+
+    } else {
+
     }
   }
 }
